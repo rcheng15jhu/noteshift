cf28264a6f4bb75968604de17bbc2990bd136605
Xiangyu Shen <xshen2006@yahoo.com>
2019-09-14 00:20:11 -0400

    typo

--- html/index.php
+++ html/index.php
@@ -27,7 +27,7 @@
             echo "Sorry, your file was not uploaded.";
             // if everything is ok, try to upload file
         } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            if (move_uploaded_file($_FILES["submittedFile"]["tmp_name"], $target_file)) {
                 echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
             } else {
                 echo "Sorry, there was an error uploading your file.";
