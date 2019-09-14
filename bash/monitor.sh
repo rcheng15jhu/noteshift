620e58ffbd22090ce784f066e5911c01ec2ff48a
shintyl <tylerjshin@gmail.com>
2019-09-14 18:49:22 +0000

    omr

--- bash/monitor.sh
+++ bash/monitor.sh
@@ -3,8 +3,8 @@ inotifywait -m /var/www/html/uploads/ -e create -e moved_to |
     while read path action file; do
 	
         echo "The file '$file' appeared in directory '$path' via '$action'"
-	
-        # do something with the file
 
+	./omr.sh $file
+	
     done
 
