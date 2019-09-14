358f554d0963eac435b6045e79d817d5119c4ab1
shintyl <tylerjshin@gmail.com>
2019-09-14 19:32:40 +0000

    ahhhhh

--- bash/omr.sh
+++ bash/omr.sh
@@ -2,4 +2,4 @@
 
 cd /home/shintyl/audiveris/
 
-./gradlew run -PcmdLineArgs="-batch,-export,-output,/var/www/html/mxl,--,/var/www/html/uploads/"$1
+./gradlew run -PcmdLineArgs="-batch,-export,-output,/var/www/html/mxl/original,--,/var/www/html/uploads/"$1
